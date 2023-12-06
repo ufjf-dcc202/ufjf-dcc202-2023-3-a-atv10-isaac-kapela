46d0a95b59291cea9fcaca9cc8dc20877a4b394e
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2023-12-05 22:29:18 -0300

    subindo no git

---
+++ joaoEMaria.js
@@ -0,0 +1,5 @@
+function getMaria() {
+    return maria;
+  }
+
+  export {getMaria}
---
+++ main.js
@@ -0,0 +1,28 @@
+let maria = 2;
+let joao = 1;
+
+ function deJoaoParaMaria() {
+  maria += joao;
+  joao = 0;
+}
+,
+
+ function deMariaParaJoao() {
+  joao += maria;
+  maria = 0;
+}
+
+ function getMaria() {
+  return maria;
+}
+ function getJoao() {
+  return joao;
+}
+
+export function setMaria(value) {
+  maria = value;
+}
+
+export function setJoao(value) {
+  joao = value;
+}
